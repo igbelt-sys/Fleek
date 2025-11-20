abc12177f7c525aa00aeebc30677c6473ac6ea93
igbelt-sys <beltramoigor1@gmail.com>
2025-11-20 02:19:58 -0300

    atualliza codigos

--- SistemaLogin/app.py
+++ SistemaLogin/app.py
@@ -3,9 +3,14 @@ import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 import re
 import dns.resolver
+import os
 
 app = Flask(__name__)
 app.secret_key = "segredo_top"
+
+# Caminho fixo do banco dentro da pasta SistemaLogin
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "users.db")
 
 # --- Validação regex ---
 def email_valido(email):
@@ -22,7 +27,7 @@ def dominio_tem_mx(email):
         return False
 
 def init_db():
-    with sqlite3.connect("users.db") as con:
+    with sqlite3.connect(DB_PATH) as con:
         cur = con.cursor()
         cur.execute("""
             CREATE TABLE IF NOT EXISTS users (
@@ -35,18 +40,15 @@ def init_db():
 
 init_db()
 
-
 @app.route("/")
 def login():
     return render_template("login.html")
-
 
 @app.route("/login", methods=["POST"])
 def login_user():
     email = request.form["email"]
     password = request.form["password"]
 
-    # validações
     if not email_valido(email):
         flash("E-mail inválido!", "error")
         return redirect("/")
@@ -55,7 +57,7 @@ def login_user():
         flash("Domínio de e-mail não existe!", "error")
         return redirect("/")
 
-    with sqlite3.connect("users.db") as con:
+    with sqlite3.connect(DB_PATH) as con:
         cur = con.cursor()
         cur.execute("SELECT email, password FROM users WHERE email = ?", (email,))
         user = cur.fetchone()
@@ -67,11 +69,9 @@ def login_user():
         flash("Email ou senha incorretos!", "error")
         return redirect("/")
 
-
 @app.route("/register")
 def register():
     return render_template("register.html")
-
 
 @app.route("/register", methods=["POST"])
 def register_user():
@@ -89,7 +89,7 @@ def register_user():
     password_hash = generate_password_hash(password)
 
     try:
-        with sqlite3.connect("users.db") as con:
+        with sqlite3.connect(DB_PATH) as con:
             cur = con.cursor()
             cur.execute("INSERT INTO users (email, password) VALUES (?, ?)", (email, password_hash))
             con.commit()
@@ -101,12 +101,10 @@ def register_user():
         flash("E-mail já está em uso!", "error")
         return redirect("/register")
 
-
 @app.route("/home")
 def home():
     if "user" in session:
         return f"Bem-vindo(a), {session['user']}!"
     return redirect("/")
-
 
 app.run(debug=True)
